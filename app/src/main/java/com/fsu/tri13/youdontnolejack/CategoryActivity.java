47cd0b2ca1a763575f248c22df7e61a547530e42
tim_ingle <timothy.ingle@hotmail.com>
2016-07-08 20:51:48 -0400

    You Don't 'Nole Jack
    COP4656 Group Project

---
+++ app/src/main/java/com/fsu/tri13/youdontnolejack/CategoryActivity.java
@@ -0,0 +1,13 @@
+package com.fsu.tri13.youdontnolejack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
---
+++ app/src/main/java/com/fsu/tri13/youdontnolejack/Player.java
@@ -0,0 +1,8 @@
+package com.fsu.tri13.youdontnolejack;
+
+/**
+ * Created by Tim on 7/4/2016.
+ */
+public class Player {
+
+}
