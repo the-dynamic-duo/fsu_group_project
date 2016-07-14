05f55a4d5c8d7f84c105e245770e0f208f68f0f7
ataylor <alt14e@my.fsu.edu>
2016-07-14 17:43:12 -0600

    Reimplement Player.java

--- app/src/main/java/com/fsu/tri13/youdontnolejack/Player.java
+++ app/src/main/java/com/fsu/tri13/youdontnolejack/Player.java
@@ -1,9 +1,89 @@
+/********************************************************************
+ Player.java
+ Adam Taylor & Timothy Ingle
+ COP-4656 - Group Project (The Dynamic Duo)
+ 07/09/2016
+
+ The Player class creates an instance of a player for the game
+ "You Don't Nole Jack"
+
+ Player() stores the player's name, ID, current score, high score,
+ and number of games played for each instance of the class.
+
+ A count of the number of players and a map of each player ID to
+ their corresponding name is shared across all instances of the class
+ *******************************************************************/
+
 package com.fsu.tri13.youdontnolejack;
 
-/**
- * Created by Tim on 7/4/2016.
- */
-public class Player {
-//test
-    Player() {}
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class Player
+{
+    private int currentScore, highScore, gamesPlayed, playerID;
+    private String playerName;
+
+    private static int numPlayers = 0;
+    private static Map<Integer,String> playerMap = new LinkedHashMap<Integer, String>();
+
+    public Player(String inputName)
+    {
+        ++numPlayers;
+
+        if ( BuildConfig.DEBUG && searchPlayerMapForName(inputName) )
+        {
+            throw new AssertionError( "Ensure an existing name isn't passed in to the function by "
+                                    + "checking if the name exists using searchPlayerMapForName()");
+        }
+
+        playerName = inputName;
+        initializePlayer();
+    }
+
+    /* TODO: We need some sort of destructor for the class to decrement numPlayers and remove extra
+             entries in playerMap. For now we can call close() if a certain player is not continuing
+             with the game. It's too bad Java doesn't have an actual destructor... */
+    public void close()
+    {
+        --numPlayers;
+        for (int i = numPlayers; i < playerMap.size(); ++i)
+        {
+            playerMap.remove(i-1);
+        }
+    }
+
+    private void initializePlayer()
+    {
+        currentScore = 0;
+        highScore = 0;
+        gamesPlayed = 0;
+        playerID = numPlayers;
+        playerMap.put(playerID, playerName);
+    }
+
+    public void resetAllTracking()
+    {
+        currentScore = 0;
+        highScore = 0;
+        gamesPlayed = 0;
+    }
+
+    public void incrementCurrentScore() {++currentScore;}
+
+    public void   setCurrentScore(int score)    {currentScore = score;}
+    public int    getCurrentScore()             {return currentScore;}
+    public void   setHighScore(int score)       {highScore = score;}
+    public int    getHighScore()                {return highScore;}
+    public void   setGamesPlayed(int numPlayed) {gamesPlayed = numPlayed;}
+    public int    getGamesPlayed()              {return gamesPlayed;}
+    public void   setPlayerName(String name)    {playerName = name;}
+    public String getPlayerName()               {return playerName;}
+
+    // The following are internally managed and should have no set methods
+    public int getPlayerID()                   {return playerID;}
+    public int getNumPlayers()                 {return numPlayers;}
+
+    // Search map for the given String value and return true if a name is taken.
+    public boolean searchPlayerMapForName(String name) {return playerMap.containsValue(name);}
 }
